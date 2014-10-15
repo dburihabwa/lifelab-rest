b0973484c0b6381f8022bc7a3a60cd66f9ea1273
Dorian Burihabwa <dorian@burihabwa.com>
2014-10-15 09:38:44 +0200

    Added server setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# Drop the database
+php app/console doctrine:database:drop
+# Create the database
+php app/console doctrine:database:create
+# Generate getters/setters for your entities
+php app/console doctrine:generate:entities LifeLabRestBundle 
+# Update the database schema
+php app/console doctrine:schema:update --force 
+# List all available routes
+php app/console route:debug 
+# Start the server
+php app/console server:run
