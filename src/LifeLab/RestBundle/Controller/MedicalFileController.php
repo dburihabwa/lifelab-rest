a9b54caa6932e120dfa4214ad2ca09ed4131fd60
Dorian Burihabwa <dorian@burihabwa.com>
2014-11-12 13:09:57 +0100

    Added POST/ on treatments
    Added POST capability on treatments

--- src/LifeLab/RestBundle/Controller/MedicalFileController.php
+++ src/LifeLab/RestBundle/Controller/MedicalFileController.php
@@ -3,10 +3,15 @@
 namespace LifeLab\RestBundle\Controller;
 
 use LifeLab\RestBundle\Entity\MedicalFile;
+use LifeLab\RestBundle\Entity\Medicine;
+use LifeLab\RestBundle\Entity\Treatment;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+use JMS\Serializer\SerializerBuilder;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
@@ -30,5 +35,59 @@ class MedicalFileController extends FOSRestController
         $view = $this->view($medicalFile, $statusCode);
         return $this->handleView($view);
     }
-}
 
+    public function postTreatmentsAction($id, Request $request) {
+        $repository = $this->getDoctrine()->getManager()->getRepository('LifeLabRestBundle:MedicalFile');
+        $medicalFile = $repository->find($id);
+        if ($medicalFile == NULL) {
+            throw new NotFoundHttpException('not found');
+        }
+        $json = $request->getContent();
+        print_r($json);
+        $serializer = SerializerBuilder::create()->build();
+        $treatment = $serializer->deserialize($json, 'LifeLab\RestBundle\Entity\Treatment', 'json');
+        if ($treatment->getDate() == NULL) {
+            $statusCode = 400;
+            $view = $this->view("Date field must be defined!", $statusCode);
+            return $this->handleView($view);
+        }
+        if ($treatment->getFrequency() == NULL) {
+            $statusCode = 400;
+            $view = $this->view("Frequency field must be defined!", $statusCode);
+            return $this->handleView($view);
+        }
+        $treatment->setMedicalFile($medicalFile);
+        //Test if medicine exists in database
+        $medicine = $treatment->getMedicine();
+        if ($medicine) {
+            $repository = $this->getDoctrine()->getManager()->getRepository('LifeLabRestBundle:Medicine');
+            $medicineId = $medicine->getId();
+            $actualMedicine = $repository->find($medicineId);
+            if (!$actualMedicine) {
+                $statusCode = 400;
+                $view = $this->view($treatment, $statusCode);
+                return $this->handleView($view);
+            }
+            $treatment->setMedicine($actualMedicine);
+        }
+        //Test if prescription exists in the database
+        $prescription = $treatment->getPrescription();
+        if ($prescription) {            
+            $repository = $this->getDoctrine()->getManager()->getRepository('LifeLabRestBundle:Prescription');
+            $prescriptionId = $prescription->getId();
+            $actualPrescription = $repository->find($prescriptionId);
+            if (!$actualPrescription) {
+                $statusCode = 400;
+                $view = $this->view($treatment, $statusCode);
+                return $this->handleView($view);
+            }
+            $treatment->setPrescription($actualPrescription);
+        }
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($treatment);
+        $em->flush();
+        $statusCode = 200;
+        $view = $this->view($treatment, $statusCode);
+        return $this->handleView($view);
+    }
+}
---
+++ src/LifeLab/RestBundle/Tests/Controller/MedicalFileControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace LifeLab\RestBundle\Tests\Controller;
+
+use LifeLab\RestBundle\Entity\Treatment;
+use LifeLab\RestBundle\Entity\Medicine;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+use JMS\Serializer\SerializerBuilder;
+
+
+class MedicalFileControllerTest extends WebTestCase {
+
+	private function getTreatment() {
+		$treatment = new Treatment();
+		$treatment->setDate(new \DateTime());
+		$treatment->setFrequency("6 fois par jour");
+		$treatment->setUnits(23);
+		$medJSON = '{
+			"id" : 1
+		}';
+		$serializer = SerializerBuilder::create()->build();
+		$medicine = $serializer->deserialize($medJSON, 'LifeLab\RestBundle\Entity\Medicine', 'json');
+		$treatment->setMedicine($medicine);
+		return $treatment;
+	}
+	
+	public function testPostTreatments() {
+		$client = static::createClient();
+		$treatment = $this->getTreatment();
+		$serializer = SerializerBuilder::create()->build();
+		$jsonContent = $serializer->serialize($treatment, 'json');
+		$client->request('POST', 
+			'/files/1/treatments', 
+			array(),
+			array(),
+			array('Content-Type' => 'application/json'),
+			$jsonContent);
+		$result = $client->getResponse()->getContent();
+		$newTreatment = $serializer->deserialize($result, 'LifeLab\RestBundle\Entity\Treatment', 'json');
+		print_r($result);
+		$this->assertTrue($client->getResponse()->headers->contains('Content-Type', 'application/json'));
+		$this->assertObjectHasAttribute('id', $newTreatment);
+	}
+}
