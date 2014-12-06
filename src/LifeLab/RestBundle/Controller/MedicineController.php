71a2f286116b98f201a034000173aaa6ee01204b
Dorian Burihabwa <dorian@burihabwa.com>
2014-12-06 15:50:33 +0100

    Added search API for medicines
    Added a search endpoint for medicines. The search is done by trying to
    match the keyword given with the name of the medicine.

--- src/LifeLab/RestBundle/Controller/MedicineController.php
+++ src/LifeLab/RestBundle/Controller/MedicineController.php
@@ -2,11 +2,13 @@
 
 namespace LifeLab\RestBundle\Controller;
 
+use LifeLab\RestBundle\Controller\AbstractController;
 use LifeLab\RestBundle\Entity\Medicine;
 
-use LifeLab\RestBundle\Controller\AbstractController;
-
+use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
+
+use Symfony\Component\HttpFoundation\Request;
 
 
 /**
@@ -19,6 +21,30 @@ class MedicineController extends AbstractController {
 
     protected function getEntityName() {
         return 'LifeLab\RestBundle\Entity\Medicine';
+    }
+
+    /**
+     * Search for medicines by name.
+     * A limit might be specified using a query string parameter (ie: ?limit=<limit>)
+     * @Get("/medicines/search/{keyword}")
+     */
+    public function searchAction(Request $request, $keyword) {
+	    $em = $this->getDoctrine()->getManager();
+	    $limitParameter = $request->query->get('limit');
+	    $limit = 25;
+	    if ($limit) {
+		    $interpretedValue = intval($limitParameter, 10);
+		    if ($interpretedValue > 0) {
+			    $limit = $interpretedValue;
+		    }
+	    }
+	    $query = $em->createQuery('SELECT m FROM LifeLabRestBundle:Medicine m WHERE m.name LIKE :keyword');
+	    $query->setParameter('keyword', '%' . $keyword . '%');
+	    $query->setMaxResults($limit);
+	    $medicines = $query->getResult();
+	    $statusCode = 200;
+	    $view = $this->view($medicines, $statusCode);
+	    return $this->handleView($view);
     }
 }
 
