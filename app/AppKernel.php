7d429c7a01466f82cd9ba7d5a7b4a331b30a5fb2
Dorian Burihabwa <dorian@burihabwa.com>
2014-12-31 16:18:56 +0100

    fosuserbundle import
    Added fosuserbundle for user management (ie. regsitration, login and
    logout).

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new LifeLab\RestBundle\LifeLabRestBundle()
         );
 
