432d65b4ec3daa04606318ad33d8e572cb112806
Dorian Burihabwa <dorian@burihabwa.com>
2015-02-08 21:43:30 +0100

    Added treatment API to confirm a treatment intake to the database
    Added treatment API to confirm a treatment intake to the database by
    signaling the time of the intake on a specific treatment.

--- src/LifeLab/RestBundle/Controller/TreatmentController.php
+++ src/LifeLab/RestBundle/Controller/TreatmentController.php
@@ -2,11 +2,15 @@
 
 namespace LifeLab\RestBundle\Controller;
 
+use LifeLab\RestBundle\Controller\AbstractController;
 use LifeLab\RestBundle\Entity\Treatment;
 
-use LifeLab\RestBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 use FOS\RestBundle\Controller\Annotations\RouteResource;
+
+use JMS\Serializer\SerializerBuilder;
 
 
 /**
@@ -20,7 +24,31 @@ class TreatmentController extends AbstractController {
     protected function getEntityName() {
         return 'LifeLab\RestBundle\Entity\Treatment';
     }
+
+    public function postIntakesAction($id, Request $request) {
+        $treatment = $this->getEntity($id);
+        if ($treatment == NULL) {
+            throw new NotFoundHttpException('Treatment not found');
+        }
+        $json = $request->getContent();
+        $serializer = SerializerBuilder::create()->build();
+        $intake = $serializer->deserialize($json, 'LifeLab\RestBundle\Entity\Intake', 'json');
+        if ($intake == NULL) {
+            $statusCode = 400;
+            $view = $this->view('intake body is missing!', $statusCode);
+            return $this->handleView($view);
+        }
+        if ($intake->getTime() == NULL) {
+            $statusCode = 400;
+            $view = $this->view('time is missing!', $statusCode);
+            return $this->handleView($view);
+        }
+        $intake->setTreatment($treatment);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($intake);
+        $entityManager->flush();
+        $statusCode = 200;
+        $view = $this->view($intake, $statusCode);
+        return $this->handleView($view);
+    }
 }
-
-
-
---
+++ src/LifeLab/RestBundle/Entity/Intake.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace LifeLab\RestBundle\Entity;
+
+
+use JMS\Serializer\Annotation\Type;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Intake
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Intake
+{
+    /**
+     * @var integer     
+     * @Type ("integer")
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @Type ("DateTime")
+     * @var \DateTime
+     *
+     * @ORM\Column(name="time", type="datetime")
+     */
+    private $time;
+
+    /**
+     * @Type ("LifeLab\RestBundle\Entity\Treatment")
+     * @ORM\ManyToOne(targetEntity="Treatment")
+     * @ORM\JoinColumn(name="treatment_id", referencedColumnName="id", nullable=false)
+     */
+    private $treatment;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set time
+     *
+     * @param \DateTime $time
+     * @return Intake
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    /**
+     * Get time
+     *
+     * @return \DateTime 
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * Set treatment
+     *
+     * @param \LifeLab\RestBundle\Entity\Treatment $treatment
+     * @return Intake
+     */
+    public function setTreatment(\LifeLab\RestBundle\Entity\Treatment $treatment)
+    {
+        $this->treatment = $treatment;
+
+        return $this;
+    }
+
+    /**
+     * Get treatment
+     *
+     * @return \LifeLab\RestBundle\Entity\Treatment 
+     */
+    public function getTreatment()
+    {
+        return $this->treatment;
+    }
+}
